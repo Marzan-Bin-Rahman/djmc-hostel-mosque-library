9f24d61a1d15b5cd65c52c64ed3a38e523936f86
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2025-10-01 05:01:55 -0400

    about us done

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <div className="flex justify-center items-center min-h-full py-12 border-2 border-[#d3c8b8] bg-[#f8ead4] rounded-xl mb-12 mt-32 mx-4">
+      <div className="w-4/5 p-5">
+        <h2 className="text-center mb-5 text-4xl md:text-7xl font-extrabold ">About Us</h2>
+        <p className="text-justify leading-relaxed text-xl">
+          The Hostel Mosque Library of Dinajpur Medical College serves as a center for both worship and learning. Students gather here five times daily to perform their prayers in congregation, fostering a sense of unity and spiritual growth. Alongside the prayer space, the mosque hosts a library with over 250 books, offering resources for both religious and academic enrichment. Regular Quran learning courses are also arranged, giving students the opportunity to deepen their understanding of the Holy Quran while strengthening their connection to faith. This unique blend of devotion, knowledge, and community makes the Hostel Mosque Library a cherished place for students.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
--- src/pages/PhaseFour.jsx
+++ src/pages/PhaseFour.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PhaseFour = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="p-15 border-2 border-[#d3c8b8] bg-[#f8ead4] rounded-lg shadow-lg text-4xl font-bold text-gray-700">
+      <div className="p-15 border-2 border-[#d3c8b8] bg-[#f8ead4] rounded-lg text-4xl font-bold text-gray-700">
         <h2 className="m-0">There is nothing to see here!</h2>
       </div>
     </div>
