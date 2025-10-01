3951318a0371a77fbf0a0fd60375aad14ea573bf
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2025-10-01 04:12:32 -0400

    phases are done

--- src/pages/PhaseFour.jsx
+++ src/pages/PhaseFour.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const PhaseFour = () => {
   return (
-    <div>PhaseFour</div>
+    <div className="flex justify-center items-center h-screen">
+      <div className="p-15 border-2 border-[#d3c8b8] bg-[#f8ead4] rounded-lg shadow-lg text-4xl font-bold text-gray-700">
+        <h2 className="m-0">There is nothing to see here!</h2>
+      </div>
+    </div>
   )
 }
 
