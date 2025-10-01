fbb7c8da891ce46516125bcdc11902e9cb600076
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2025-10-01 05:39:11 -0400

    contact done

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -7,6 +7,9 @@ const AboutUs = () => {
         <h2 className="text-center mb-5 text-4xl md:text-7xl font-extrabold ">About Us</h2>
         <p className="text-justify leading-relaxed text-xl">
           The Hostel Mosque Library of Dinajpur Medical College serves as a center for both worship and learning. Students gather here five times daily to perform their prayers in congregation, fostering a sense of unity and spiritual growth. Alongside the prayer space, the mosque hosts a library with over 250 books, offering resources for both religious and academic enrichment. Regular Quran learning courses are also arranged, giving students the opportunity to deepen their understanding of the Holy Quran while strengthening their connection to faith. This unique blend of devotion, knowledge, and community makes the Hostel Mosque Library a cherished place for students.
+          <br />
+          <br />
+          Every <span className='font-extrabold'>Sunday after Maghrib prayer</span> Dars-E-Quran is conducted by Mawlana Rafiul Islam, a prominent scholar of Dinajpur. To get the previous Dars-E-Quran lecture you can head to the Dars section of this website.
         </p>
       </div>
     </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { FaMapMarkerAlt, FaEnvelope, FaTelegramPlane } from 'react-icons/fa';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="flex justify-center mt-32 mb-12 mx-4">
+      <div className="border-2 border-[#d3c8b8] bg-[#f8ead4] rounded-xl p-8 flex flex-col md:flex-row w-full">
+        {/* Left Column - Logo */}
+        <div className="flex justify-center items-center mb-6 md:mb-0 md:mr-8">
+          <img
+            src="/src/assets/logo.png"
+            alt="Logo"
+            className="w-full"
+          />
+        </div>
 
-export default Contact
+        {/* Right Column - Contact Info */}
+        <div className="flex flex-col justify-center content-center space-y-4">
+          <div className="flex items-center">
+            <FaMapMarkerAlt className="text-xl text-gray-700 mr-4" />
+            <span className="text-gray-700">Dr. Yousuf Ali Boys Hostel, Dinajpur Medical College</span>
+          </div>
+          <div className="flex items-center">
+            <FaEnvelope className="text-xl text-gray-700 mr-4" />
+            <span className="text-gray-700">email@example.com</span>
+          </div>
+          <div className="flex items-center">
+            <FaTelegramPlane className="text-xl text-gray-700 mr-4" />
+            <span className="text-gray-700">@telegramhandle</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
